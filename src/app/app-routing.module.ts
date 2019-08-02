cecd3979936598fae3cd6d1f07443ef8e5a726cc
GABRIEL MACHADO DE ANDRADE <gabriel.mandrade1@senacsp.edu.br>
2019-08-02 19:30:14 -0300

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeLayoutComponent } from './layout/home-layout/home-layout.component';
+import { DashboardComponent } from './core/dashboard/dashboard.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+{
+  path: '',
+  component: HomeLayoutComponent,
+  canActivate: [],
+  children: [
+  {path: 'dashboard', component: DashboardComponent}
+  ]
+}
+
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
